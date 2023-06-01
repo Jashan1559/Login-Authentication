2542581ad78d46e2d580c89069da0897561e6321
Jashan1559 <122783494+Jashan1559@users.noreply.github.com>
2023-06-01 12:08:18 +0530

    Update forgot password.js

--- forgot password.js
+++ forgot password.js
@@ -16,17 +16,24 @@ function forgotpass(){
     mail.push(em);
     pass.push(pw);
 
-    console.log(uname);
-    console.log(mail);
-    console.log(pass);
+    console.log("array of username: " + uname);
+    console.log("array of emails: " + mail);
+    console.log("array of password: " + pass);
+    console.log("input of user name: " + name);
+    console.log("input of user email: " + eml);
+    console.log("username in local storage: " + nm);
+    console.log("password in local storage: " + pw);
+    console.log("email in local storage: " + em);
 
-    for(let i = 0; i < uname.length; i++ ){
-        if(name == uname[i] && eml == mail[i]){
+
+
+    for (let i=0; i<uname.length; i++){
+        if (name == uname[i] && eml == mail[i]){
             alert("Your password is " + pass[i]);
         }
         else{
-            alert("Invalid Username or email !");
-            break;
+            document.getElementById("error").innerHTML="Enter valid username/password!"
         }
     }
+
 }
